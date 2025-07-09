acf46acc300d1f1d548d375c5cc929c0d1098235
Rahul-dev02 <rahulroa14320@gmail.com>
2025-07-09 12:41:42 +0530

    first-class-letvarconst

---
+++ var.js
@@ -0,0 +1,16 @@
+
+const AccountId=2109
+let AccountEmail="Rahul@123"
+var accountname="Rahul yadav"
+ let accountState; 
+
+
+console.table([AccountId,AccountEmail, accountname,accountState]);
+
+// AccountId=2000
+AccountEmail="Lata@123"
+accountname="Lata"
+accountState="up"
+
+console.table([AccountId,AccountEmail, accountname, accountState]);
+ 
