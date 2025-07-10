0205c2e3c0540fc0ee5f416f9f1536a70cf9337d
Rahul-dev02 <rahulroa14320@gmail.com>
2025-07-10 14:24:04 +0530

    data_converstion

---
+++ type_convertion.js
@@ -0,0 +1,23 @@
+
+// let score=null
+
+// console.log(typeof score)
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+
+// console.log(valueInNumber)
+
+//  let str=123
+//  console.log(typeof str)
+
+//  let newStr=String(str)
+
+//  let strConcat=(str+"ra")
+//  console.log(typeof strConcat)
+//  console.log(strConcat)
+
+
+let isLoggedIn=1
+let bolleanLoggedin=Boolean(isLoggedIn)
+console.log(bolleanLoggedin)
