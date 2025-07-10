25b8092c3110b24115e0db04182a7ee0a71e19e0
Rahul-dev02 <rahulroa14320@gmail.com>
2025-07-10 17:55:03 +0530

    comprision of data type

--- type_convertion.js
+++ type_convertion.js
@@ -20,4 +20,18 @@
 
 let isLoggedIn=1
 let bolleanLoggedin=Boolean(isLoggedIn)
-console.log(bolleanLoggedin)
+// console.log(bolleanLoggedin)
+
+let value=3;
+let negValue=-value
+// console.log(negValue)
+
+let gameCounter=100
+
+let newGameCounter=gameCounter++
+// console.log(gameCounter)
+// console.log(newGameCounter)
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
